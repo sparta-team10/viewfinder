b49e9f0cc037afbe905bff5846500aa221b960ad
soyoung <soyoung.yang94@gmail.com>
2024-06-19 20:23:58 +0900

    feat : comment - CommentService - CommentController - CommentRepository - Dto

--- src/main/java/com/sparta/viewfinder/dto/CommentRequestDto.java
+++ src/main/java/com/sparta/viewfinder/dto/CommentRequestDto.java
@@ -1,4 +1,8 @@
 package com.sparta.viewfinder.dto;
 
+import lombok.Getter;
+
+@Getter
 public class CommentRequestDto {
+    private String content;
 }
--- src/main/java/com/sparta/viewfinder/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/viewfinder/dto/CommentResponseDto.java
@@ -1,4 +1,19 @@
 package com.sparta.viewfinder.dto;
 
+import com.sparta.viewfinder.entity.Comment;
+import lombok.Getter;
+
+@Getter
 public class CommentResponseDto {
+    private final Long id;
+    private final Long userId;
+    private final Long postId;
+    private final String content;
+
+    public CommentResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.userId = comment.getUserId();
+        this.postId = comment.getPostId();
+        this.content = comment.getContent();
+    }
 }
