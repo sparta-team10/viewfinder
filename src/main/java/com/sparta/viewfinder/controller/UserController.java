aedb8c7a141b908f0b78d2dccc4047f7a2b8cd80
Cherryshine <jee020205@gmail.com>
2024-06-19 18:11:04 +0900

    refactor : usercontroller 주석 제거

--- src/main/java/com/sparta/viewfinder/controller/UserController.java
+++ src/main/java/com/sparta/viewfinder/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-//@RequestMapping("/sign-up")
 public class UserController {
 
     private final UserService userService;
