01f19b1aeb0e634a0c2e997bc7dd2a2658f86267
silicao3o <zergskybmw@gmail.com>
2024-06-19 16:27:54 +0900

    feat : login/logout

--- src/main/java/com/sparta/viewfinder/service/UserService.java
+++ src/main/java/com/sparta/viewfinder/service/UserService.java
@@ -1,4 +1,30 @@
 package com.sparta.viewfinder.service;
 
+import com.sparta.viewfinder.dto.LoginRequestDto;
+import com.sparta.viewfinder.entity.User;
+import com.sparta.viewfinder.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private Long id;
+
+    public boolean login(LoginRequestDto requestDto){
+        User findUser = userRepository.findByUsername(requestDto.getUsername()).get();
+        if(findUser == null || !findUser.getPassword().equals(requestDto.getPassword())){
+            return false;
+        }
+        return true;
+    }
+
+    @Transactional
+    public boolean logout(Long id){
+        this.id = id;
+        User user = userRepository.findById(id).get();
+        return user.logout();
+    }
 }
