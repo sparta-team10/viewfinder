9868148884e7a4ce21fdc9df9426a5dce02947d7
soyoung <soyoung.yang94@gmail.com>
2024-06-20 16:48:55 +0900

    fix : deletePost

--- src/main/java/com/sparta/viewfinder/entity/Post.java
+++ src/main/java/com/sparta/viewfinder/entity/Post.java
@@ -1,7 +1,6 @@
 package com.sparta.viewfinder.entity;
 
 import com.sparta.viewfinder.dto.PostRequestDto;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -14,12 +13,10 @@ import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @Table(name = "post")
 public class Post extends Timestamped{
