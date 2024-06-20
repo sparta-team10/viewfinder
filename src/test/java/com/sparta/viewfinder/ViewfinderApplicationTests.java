a3998713ea492cee578c656680e6c0321bea04db
soyoung <soyoung.yang94@gmail.com>
2024-06-20 16:33:43 +0900

    feat : setUserAndPostTest

--- src/test/java/com/sparta/viewfinder/ViewfinderApplicationTests.java
+++ src/test/java/com/sparta/viewfinder/ViewfinderApplicationTests.java
@@ -1,13 +1,36 @@
 package com.sparta.viewfinder;
 
+import com.sparta.viewfinder.entity.Post;
+import com.sparta.viewfinder.entity.User;
+import com.sparta.viewfinder.repository.PostRepository;
+import com.sparta.viewfinder.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class ViewfinderApplicationTests {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
     @Test
-    void contextLoads() {
+    @DisplayName("User & Post 더미 테스트 추가")
+    @Transactional
+    @Rollback(value = false)
+    void setUserAndPost() {
+        User user = new User();
+        userRepository.save(user);
+
+        Post post = new Post();
+        post.setUser(user);
+        postRepository.save(post);
     }
 
 }
