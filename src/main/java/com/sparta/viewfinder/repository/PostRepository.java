63f98c0c382df8d07273c60c682c6a37695c9bb6
Cherryshine <hi.im.cherryshine@gmail.com>
2025-05-07 03:29:03 +0900

    add: chatting feature

--- src/main/java/com/sparta/viewfinder/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.viewfinder.repository;
-
-import com.sparta.viewfinder.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
